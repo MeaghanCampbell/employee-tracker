a147c185fc97d317f6870b7d9f766403240501b6
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-03-06 12:39:20 -0600

    database connection troubleshooting

---
+++ db/database.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql2');
+ 
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3001,
+  // Your MySQL username
+  user: 'root',
+  // Your MySQL password
+  password: 'singstar73',
+  database: 'employees'
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId);
+});
+
+// class DB {
+//     constructor(connection) {
+//        this.connection = connection
+//     }
+// }
+
+module.exports = connection
+
--- index.js
+++ index.js
@@ -1,10 +1,100 @@
 const inquirer = require('inquirer')
 const cTable = require('console.table');
+const connection = require('./db/database')
 
-const mysql = require('mysql2');
- 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'test'
-});
+afterConnection = () => {
+  return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'what',
+      message: 'What would you like to do?',
+      choices: [
+        {
+          name: "View all departments",
+          value: "viewDepartments"
+        },
+        {
+          name: "View all roles",
+          value: "viewRoles"
+        },
+        {
+          name: "View all employees",
+          value: "viewEmployees"
+        },
+        {
+          name: "Add a department",
+          value: "addDepartment"
+        },
+        {
+          name: "Add a role",
+          value: "addRole"
+        },
+        {
+          name: "Add an employee",
+          value: "addEmployee"
+        },
+        {
+          name: "Update employee role",
+          value: "updateEmployeeRole"
+        }
+      ]
+    }
+  ])
+  .then(answer => {
+    if (answer.what === 'viewDepartments') {
+      console.log('view departments')
+    } else if (answer.what === 'viewRoles') {
+      console.log('view roles')
+    } else if (answer.what === 'viewEmployees') {
+      console.log('view employees')
+    } else if (answer.what === 'addDepartment') {
+      return inquirer.prompt([
+        {
+          type: 'input',
+          name: 'newDepartment',
+          message: 'what is the name of the department?'
+        }
+      ])
+      .then(answer => {
+        // add dept name to the database
+        console.log(answer)
+      })
+    } else if (answer.what === 'addRole') {
+      return inquirer.prompt([
+        {
+          type: 'input',
+          name: 'newRole',
+          message: 'what is the name of the role?'
+        },
+        {
+          type: 'input',
+          name: 'roleSalary',
+          message: 'what is the salary for the role?'
+        }
+      ])
+      .then(answer => {
+        // add role info to the database
+        console.log(answer)
+      })
+    } else if (answer.what === 'addEmployee') {
+      return inquirer.prompt([
+        {
+          type: 'input',
+          name: 'employeeFirstName',
+          message: 'Please enter employees first name.'
+        },
+        {
+          type: 'input',
+          name: 'employeeLastName',
+          message: 'Please enter employees last name'
+        }
+      ])
+      .then(answer => {
+        // add employee info to the database
+        console.log(answer)
+      })
+    }
+  })  
+}
+
+afterConnection()
